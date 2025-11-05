b4f36b49cc8fdc753d30647aac48458b727c344b
Rafael Machado <rafaelmachadocd37@gmail.com>
2025-11-04 22:26:57 -0300

    Minor changes

--- ex05.ts
+++ ex05.ts
@@ -5,39 +5,54 @@ Faça um programa e teste interativamente:
 Crie 2 macacos
 Alimente-os com 3 alimentos diferentes e verificando o conteúdo do estômago a cada refeição.
 Experimente fazer com que um macaco coma o outro. É possível criar um macaco canibal?*/
-
 class macaco {
   nome: string
-  estomago[]: string
+  bucho: string[]
 
-  constructor(nome: string, estomago: string){
+  constructor(nome: string, bucho: string[]){
     this.nome = nome
-    this.estomago = estomago
-
+    this.bucho = bucho
   }
 
-  comer(a: string){
-    const 
-    
-
-
+  comer(){
+    console.log(`O ${macaco} está comendo ${this.bucho}`)
   }
 
-
-  verEstomago(a: string){}
-
-
-  digerir(a: string){
-    this.estomago
-
-
+  verBucho(){
+    if(this.bucho.length > 0){
+      console.log(`O bucho do macaco tem ${this.bucho}`)
+    } else {
+      console.log(`O bucho do macaco não tem nada`)
+    }
   }
 
-
-
-
-
-
-
-
+  digerir(){
+    if(this.bucho.length > 0){
+      function mastigando(){
+        console.log(`O ${macaco} está mastigando...`)
+      }
+      const repetindo = setInterval(mastigando, 2000)
+      setTimeout(() => {
+        clearInterval(repetindo);
+        console.log(`O ${macaco} terminou de digerir!`)
+      }, 6000);
+    } else {
+      console.log(`[ERROR] Não é possível digerir sem conteúdo no bucho`)
+    }
+  }
 }
+
+const macaco1 = new macaco("Macaquinho", ["banana", "teste"])
+macaco1.comer()
+macaco1.digerir()
+macaco1.verBucho()
+
+const macaco2 = new macaco("Macacão", ["Maçã", "teste2"])
+macaco2.comer()
+macaco2.digerir()
+macaco2.verBucho()
+
+const macaco3 = new macaco ("Macaco", ["Abacaxi", macaco1])
+macaco3.comer()
+macaco3.digerir()
+macaco3.verBucho()
