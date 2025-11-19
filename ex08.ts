ec5ea95b1c2a0c52ff7fa83d5b9c410e53d61d27
Rafael Machado <rafaelmachadocd37@gmail.com>
2025-11-18 21:51:42 -0300

    Minor changes

---
+++ ex08.ts
@@ -0,0 +1,7 @@
+/*Crie uma classe que modele um funcionário.
+Um empregado tem um nome (String) e um salário (Double)
+Escreva um construtor com dois parâmetros (nome e salário)
+Métodos para devolver nome, salário, 
+aumentar salário (porcentualDeAumento) que aumente o 
+salário do funcionário em uma certa porcentagem.
+Escreva um pequeno programa que teste sua classe.*/
