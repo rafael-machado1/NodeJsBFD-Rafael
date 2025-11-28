e026ec6fcde2b437bb2d2b606b8ef7f4db07797f
Rafael Machado <rafaelmachadocd37@gmail.com>
2025-11-28 16:08:47 -0300

    Minor changes

--- ex09.ts
+++ ex09.ts
@@ -4,9 +4,16 @@ Cada produto tem um preço e uma quantidade em estoque.
 Um pedido de um cliente é composto de 
 itens, onde cada item especifica o produto que o cliente deseja e a respectiva quantidade. 
 Esse pedido pode ser pago em dinheiro, cheque ou cartão."*/
-const item = [
-  {nomeItem: "string", idItem: 123, qtdItem: 321},
-  {nomeItem: "teste", idItem: 124, qtdItem: 12},
+interface estoqueItem {
+  nomeItem: string
+  qtdItem: number
+  precoItem: number
+}
+
+const item: estoqueItem[] = [
+  {nomeItem: "maca", qtdItem: 5, precoItem: 1},
+  {nomeItem: "manga", qtdItem: 4, precoItem: 4},
+  {nomeItem: "tomate", qtdItem: 3, precoItem: 3},
 ]
 
 class pedidoCliente {
@@ -18,11 +25,36 @@ class pedidoCliente {
     this.numPedido = 0
   }
 
-  fazerPedido(produto: typeof item):void{
-    this.numPedido += 1
-    produto = item.nomeItem 
-    console.log(`O produto pedido foi ${}`)
+    fazerPedido(produto: string, quantidade: number):void{
+      this.numPedido += 1
+    if (quantidade <= 0) {
+      console.log(`[ERRO] Digite uma quantidade válida`)
+      return;
+    }
 
+    let itemEncontrado!: estoqueItem
+
+      for (const itemAtual of item) {
+        if (produto == itemAtual.nomeItem){
+          itemEncontrado = itemAtual
+          break;
+        }
+      }
+
+      if (!itemEncontrado){
+        console.log(`[ERRO] O produto ${produto} não foi encontrado, tente novamente!`)
+        return;
+      }
+
+      console.log(`[ENCONTRADO] O produto foi encontrado no estoque, sua quantidade é de ${itemEncontrado.qtdItem} unidades`)
+      
+      if (itemEncontrado.qtdItem >= quantidade){
+        itemEncontrado.qtdItem -= quantidade
+        console.log(`[SUCESSO] Pedido de ${quantidade} unidades feito com sucesso`)
+        console.log(`[ATUALIZAÇÃO] A quantidade do item ${produto} no estoque agora é ${itemEncontrado.qtdItem}`)
+      } else {
+        console.log(`[ERRO] Quantidade inválida, o estoque tem somente ${itemEncontrado.qtdItem} unidades`)
+      }
   }
 
   metodoPagamento(metodo: string){}
@@ -30,30 +62,5 @@ class pedidoCliente {
 }
 
 //----- Testes -----\\
-/*interface estoqueItem {
-  nomeItem: string
-  qtdItem: number
-}
 
-
-
-const item: estoqueItem[] = [
-  {nomeItem: "maca", qtdItem: 5},
-  {nomeItem: "manga", qtdItem: 4},
-  {nomeItem: "tomate", qtdItem: 3},
-]
-
-function fazerPedido(produto: string, quantidade: number):void{
-  if (produto) {
-    for (const index of item) {
-      if (produto == produto.nomeItem) {
-        console.log(`[BUSCA] Seu produto foi encontrado`)
-      } else {
-        console.log(`[BUSCA] Seu produto não foi encontrado, tente novamente!`)
-      }
-    }
-  }
-  produto.qtdItem - quantidade
-}
-
-fazerPedido ("maca", 5)
+pedidoCliente.fazerPedido ("maca", 5)
